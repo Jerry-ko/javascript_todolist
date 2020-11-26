84b226aa70cc385ddf67cb88aafd07f07dcaf368
Minju Ko <70134713+Jerry-ko@users.noreply.github.com>
2020-11-26 17:22:46 +0900

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,17 @@
+const clock = document.querySelector(".js-clock .clock__text");
+
+
+function getTime() {
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const time = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}`;
+    clock.innerHTML =time;
+}
+    
+function init(){
+    getTime();
+    setInterval(getTime, 1000);
+}
+
+init();
